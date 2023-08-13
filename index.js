1f5452ad7610719fa454a3c5c2d7ac81481a3120
HoaCTa <hoatavn@gmail.com>
2023-08-12 19:42:33 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,32 @@
+/*const showMore = document.querySelectorAll(".showMore"); // link with Show More text
+const text = document.querySelectorAll(".more-text"); //span
+
+for (var i = 0; i < showMore.length; i++)
+{
+    showMore[i].addEventListener('click', (e)=>{
+        text[i].classList.toggle('show-more');
+        if(showMore[i].innerText === "Show More")
+        {
+            showMore[i].innerText = "Show Less";
+        }else{
+            showMore[i].innerText = "Show More";
+        }
+    })
+}*/
+
+for (i = 0; i < document.querySelectorAll(".showMore").length; i++)
+{
+    document.querySelectorAll(".showMore")[i].addEventListener('click', (e)=>{
+        document.querySelectorAll(".more-text")[i].toggle('show-more');
+        if(document.querySelectorAll(".showMore")[i].innerText === "Show More")
+        {
+            document.querySelectorAll(".showMore")[i].innerText = "Show Less";
+        }else{
+            document.querySelectorAll(".showMore")[i].innerText = "Show More";
+        }
+    
+    })
+
+
+}
+
