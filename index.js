456b19e088526adac8d350c6481bc49ed71e2a47
HoaCTa <hoatavn@gmail.com>
2023-08-26 11:54:49 -0700

    updated features section

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ for (var i = 0; i < showMore.length; i++)
     })
 }*/
 
-for (i = 0; i < document.querySelectorAll(".showMore").length; i++)
-{
-    document.querySelectorAll(".showMore")[i].addEventListener('click', (e)=>{
-        document.querySelectorAll(".more-text")[i].toggle('show-more');
-        if(document.querySelectorAll(".showMore")[i].innerText === "Show More")
-        {
-            document.querySelectorAll(".showMore")[i].innerText = "Show Less";
-        }else{
-            document.querySelectorAll(".showMore")[i].innerText = "Show More";
-        }
+// for (i = 0; i < document.querySelectorAll(".showMore").length; i++)
+// {
+//     document.querySelectorAll(".showMore")[i].addEventListener('click', (e)=>{
+//         document.querySelectorAll(".more-text")[i].toggle('show-more');
+//         if(document.querySelectorAll(".showMore")[i].innerText === "Show More")
+//         {
+//             document.querySelectorAll(".showMore")[i].innerText = "Show Less";
+//         }else{
+//             document.querySelectorAll(".showMore")[i].innerText = "Show More";
+//         }
     
-    })
+//     })
 
 
-}
+// }
 
